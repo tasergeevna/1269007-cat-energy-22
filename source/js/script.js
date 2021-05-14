bf7d942338875fe31cbf07ec275501ef9ccb3050
Tatsiana Yasiuchenia <tsbarankina@gmail.com>
2021-05-14 21:06:39 +0300

    Добавила декоративные элементы на страницу каталога

--- source/js/script.js
+++ source/js/script.js
@@ -5,9 +5,9 @@ navToggle.classList.remove('nav-toggle--nojs');
 
 navToggle.addEventListener('click', function() {
   navMain.classList.toggle('main-nav--closed');
-});
 
-if (navMain.classList.contains('main-nav--closed')) {
-  navToggle.classList.remove('nav-toggle--closed');
-  navToggle.classList.add('nav-toggle--hamburger');
-};
+  if (navMain.classList.contains('main-nav--closed')) {
+    navToggle.classList.remove('nav-toggle--closed');
+    navToggle.classList.add('nav-toggle--hamburger');
+  };
+});
